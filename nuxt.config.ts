53b5ea5ca67f0126d7c383d332d1949f5c49bd47
unknown <bensonagala@live.com>
2023-01-30 18:03:19 +0300

    portfolio setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  
-   
     css: ['~/assets/css/main.css', "~/assets/scss/index.scss"],
     build: {
       transpile: ["mdb-vue-ui-kit"],
