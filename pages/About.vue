04d382933b3bff1524f7029bca3831e5225e1264
unknown <bensonagala@live.com>
2023-02-07 11:30:19 +0300

    reworks on some pages

--- pages/About.vue
+++ pages/About.vue
@@ -1,179 +1,82 @@
 <template>
     <Section>
       <Navbar/>
-      <div class="px-4 py-28 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
-        <div class="max-w-xl my-16 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
-      <div>
-    <!--   <p class="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-accent-400">
-        Brand new
-      </p> -->
+  <div class="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+    <div class="max-w-xl mb-10 mt-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
+      <h2 class="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto uppercase">
+        What our <span class="text-purple-600"> experience</span> and knowhow can do to you and to your business
+      </h2>
     </div>
-    <h2 class=" mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto uppercase">
- <!--      <span class="relative inline-block">
-        <svg viewBox="0 0 52 24" fill="currentColor" class="absolute top-0 left-0 z-0 hidden w-32 -mt-8 -ml-20 text-blue-gray-100 lg:w-32 lg:-ml-28 lg:-mt-10 sm:block">
-          <defs>
-            <pattern id="fdca20a0-aeb4-4caf-ba1b-4351eee42363" x="0" y="0" width=".135" height=".30">
-              <circle cx="1" cy="1" r=".7"></circle>
-            </pattern>
-          </defs>
-          <rect fill="url(#fdca20a0-aeb4-4caf-ba1b-4351eee42363)" width="52" height="24"></rect>
-        </svg> -->
-    
-      <!-- </span> -->
-      What our <span class="text-purple-600">  experience</span> and knowhow can do to you and your business
-    </h2>
-  <!--   <p class="text-base text-gray-700 md:text-lg">
-      Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque rem aperiam, eaque ipsa quae.
-    </p> -->
-  </div>
-  <div class="grid max-w-md gap-8 row-gap-10 sm:mx-auto lg:max-w-full lg:grid-cols-3">
-    <div class="flex flex-col sm:flex-row">
-      <div class="sm:mr-4">
-        <div class="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-indigo-50">
-          <svg class="w-12 h-12 text-deep-purple-accent-400" stroke="currentColor" viewBox="0 0 52 52">
-            <polygon stroke-width="3" stroke-linecap="round" stroke-linejoin="round" fill="none" points="29 13 14 29 25 29 23 39 38 23 27 23"></polygon>
-          </svg>
+    <div class="grid max-w-screen-lg gap-8 row-gap-10 mx-auto lg:grid-cols-2">
+      <div class="flex flex-col max-w-md sm:mx-auto sm:flex-row">
+        <div class="mr-4">
+          <div class="flex items-center justify-center w-12 h-12 mb-4 rounded-full bg-indigo-50">
+            <svg class="w-10 h-10 text-deep-purple-accent-400" stroke="currentColor" viewBox="0 0 52 52">
+              <polygon stroke-width="3" stroke-linecap="round" stroke-linejoin="round" fill="none" points="29 13 14 29 25 29 23 39 38 23 27 23"></polygon>
+            </svg>
+          </div>
+        </div>
+        <div>
+          <h6 class="mb-3 text-xl font-bold leading-5">We listen</h6>
+          <p class="mb-3 text-sm text-gray-900">
+            Our success in this business is not only based on superior technical aptitude, it also includes our keen ability to carefully listen to all the client’s needs and concerns in order to provide exceptional solutions to their unique challenges.  You’re the expert in your market, and we provides the technical expertise needed to get your job done right – the first time – without getting in your way.
+          </p>
+          <a href="/" aria-label="" class="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800">Learn more</a>
         </div>
       </div>
-      <div>
-        <h6 class="mb-2 font-semibold leading-5 uppercase">We listen</h6>
-        <p class="mb-3 text-sm text-gray-900">
-          Our success in this business is not only based on superior technical aptitude, it also includes our keen ability to carefully listen to all the client’s needs and concerns in order to provide exceptional solutions to their unique challenges.  You’re the expert in your market, and UTS provides the technical expertise needed to get your job done right – the first time – without getting in your way.
-        </p>
-       <!--  <ul class="mb-4 -ml-1 space-y-2">
-          <li class="flex items-start">
-            <span class="mr-1">
-              <svg class="w-5 h-5 mt-px text-deep-purple-accent-400" stroke="currentColor" viewBox="0 0 52 52">
-                <polygon stroke-width="4" stroke-linecap="round" stroke-linejoin="round" fill="none" points="29 13 14 29 25 29 23 39 38 23 27 23"></polygon>
-              </svg>
-            </span>
-            Computers
-          </li>
-          <li class="flex items-start">
-            <span class="mr-1">
-              <svg class="w-5 h-5 mt-px text-deep-purple-accent-400" stroke="currentColor" viewBox="0 0 52 52">
-                <polygon stroke-width="4" stroke-linecap="round" stroke-linejoin="round" fill="none" points="29 13 14 29 25 29 23 39 38 23 27 23"></polygon>
-              </svg>
-            </span>
-            Health
-          </li>
-          <li class="flex items-start">
-            <span class="mr-1">
-              <svg class="w-5 h-5 mt-px text-deep-purple-accent-400" stroke="currentColor" viewBox="0 0 52 52">
-                <polygon stroke-width="4" stroke-linecap="round" stroke-linejoin="round" fill="none" points="29 13 14 29 25 29 23 39 38 23 27 23"></polygon>
-              </svg>
-            </span>
-            Reference
-          </li>
-        </ul> -->
-        <a href="/" aria-label="" class="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800">Learn more</a>
-      </div>
-    </div>
-    <div class="flex flex-col sm:flex-row">
-      <div class="sm:mr-4">
-        <div class="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-indigo-50">
-          <svg class="w-12 h-12 text-deep-purple-accent-400" stroke="currentColor" viewBox="0 0 52 52">
-            <polygon stroke-width="3" stroke-linecap="round" stroke-linejoin="round" fill="none" points="29 13 14 29 25 29 23 39 38 23 27 23"></polygon>
-          </svg>
+      <div class="flex flex-col max-w-md sm:mx-auto sm:flex-row">
+        <div class="mr-4">
+          <div class="flex items-center justify-center w-12 h-12 mb-4 rounded-full bg-indigo-50">
+            <svg class="w-10 h-10 text-deep-purple-accent-400" stroke="currentColor" viewBox="0 0 52 52">
+              <polygon stroke-width="3" stroke-linecap="round" stroke-linejoin="round" fill="none" points="29 13 14 29 25 29 23 39 38 23 27 23"></polygon>
+            </svg>
+          </div>
         </div>
-      </div>
-      <div>
-        <h6 class="mb-2 font-semibold leading-5 uppercase">We know</h6>
-        <p class="mb-3 text-sm text-gray-900">
-          Our years of experience entail working with large and small companies alike.  We know that executing to our client’s needs really means providing a uniquely focused solution, while meeting their schedule and budget constraints.  The UTS Team not only includes our in-house experts, it also includes hundreds of top notch specialty partner organizations that will ‘go the distance’ to assist UTS to deliver unique solutions that get the job done right, regardless of project scale.
-        </p>
-       <!--  <ul class="mb-4 -ml-1 space-y-2">
-          <li class="flex items-start">
-            <span class="mr-1">
-              <svg class="w-5 h-5 mt-px text-deep-purple-accent-400" stroke="currentColor" viewBox="0 0 52 52">
-                <polygon stroke-width="4" stroke-linecap="round" stroke-linejoin="round" fill="none" points="29 13 14 29 25 29 23 39 38 23 27 23"></polygon>
-              </svg>
-            </span>
-            Computers
-          </li>
-          <li class="flex items-start">
-            <span class="mr-1">
-              <svg class="w-5 h-5 mt-px text-deep-purple-accent-400" stroke="currentColor" viewBox="0 0 52 52">
-                <polygon stroke-width="4" stroke-linecap="round" stroke-linejoin="round" fill="none" points="29 13 14 29 25 29 23 39 38 23 27 23"></polygon>
-              </svg>
-            </span>
-            Health
-          </li>
-          <li class="flex items-start">
-            <span class="mr-1">
-              <svg class="w-5 h-5 mt-px text-deep-purple-accent-400" stroke="currentColor" viewBox="0 0 52 52">
-                <polygon stroke-width="4" stroke-linecap="round" stroke-linejoin="round" fill="none" points="29 13 14 29 25 29 23 39 38 23 27 23"></polygon>
-              </svg>
-            </span>
-            Reference
-          </li>
-        </ul> -->
-        <a href="/" aria-label="" class="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800">Learn more</a>
-      </div>
-    </div>
-    <div class="flex flex-col sm:flex-row">
-      <div class="sm:mr-4">
-        <div class="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-indigo-50">
-          <svg class="w-12 h-12 text-deep-purple-accent-400" stroke="currentColor" viewBox="0 0 52 52">
-            <polygon stroke-width="3" stroke-linecap="round" stroke-linejoin="round" fill="none" points="29 13 14 29 25 29 23 39 38 23 27 23"></polygon>
-          </svg>
-        </div>
-      </div>
-      <div>
-        <h6 class="mb-2 font-semibold leading-5 uppercase">We do</h6>
-        <p class="mb-3 text-sm text-gray-900">
-          When you’re the leader in your field, taking calculated risks is part of the landscape. Our subject matter expertise provides our clients with extensive technological knowledge that is combined with proven real-world deployment experience that you’ll see in every project we take on.  Our extensive toolbox is wide, deep, and well equipped with expert people and systems that are ready to take on any challenge.
-        </p>
-       <!--  <ul class="mb-4 -ml-1 space-y-2">
-          <li class="flex items-start">
-            <span class="mr-1">
-              <svg class="w-5 h-5 mt-px text-deep-purple-accent-400" stroke="currentColor" viewBox="0 0 52 52">
-                <polygon stroke-width="4" stroke-linecap="round" stroke-linejoin="round" fill="none" points="29 13 14 29 25 29 23 39 38 23 27 23"></polygon>
-              </svg>
-            </span>
-            Computers
-          </li>
-          <li class="flex items-start">
-            <span class="mr-1">
-              <svg class="w-5 h-5 mt-px text-deep-purple-accent-400" stroke="currentColor" viewBox="0 0 52 52">
-                <polygon stroke-width="4" stroke-linecap="round" stroke-linejoin="round" fill="none" points="29 13 14 29 25 29 23 39 38 23 27 23"></polygon>
-              </svg>
-            </span>
-            Health
-          </li>
-          <li class="flex items-start">
-            <span class="mr-1">
-              <svg class="w-5 h-5 mt-px text-deep-purple-accent-400" stroke="currentColor" viewBox="0 0 52 52">
-                <polygon stroke-width="4" stroke-linecap="round" stroke-linejoin="round" fill="none" points="29 13 14 29 25 29 23 39 38 23 27 23"></polygon>
-              </svg>
-            </span>
-            Reference
-          </li>
-        </ul> -->
+        <div>
+          <h6 class="mb-3 text-xl font-bold leading-5">We know</h6>
+          <p class="mb-3 text-sm text-gray-900">
+            Our years of experience entail working with large and small companies alike.  We know that executing to our client’s needs really means providing a uniquely focused solution, while meeting their schedule and budget constraints. Our Team not only includes our in-house experts, it also includes hundreds of top notch specialty partner organizations that will ‘go the distance’ to assist UTS to deliver unique solutions that get the job done right, regardless of project scale.
 
-        <a href="/" aria-label="" class="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800">Learn more</a>
-      </div>
-    </div>
-    <div class="flex flex-col sm:flex-row">
-      <div class="sm:mr-4">
-        <div class="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-indigo-50">
-          <svg class="w-12 h-12 text-deep-purple-accent-400" stroke="currentColor" viewBox="0 0 52 52">
-            <polygon stroke-width="3" stroke-linecap="round" stroke-linejoin="round" fill="none" points="29 13 14 29 25 29 23 39 38 23 27 23"></polygon>
-          </svg>
+          </p>
+          <a href="/" aria-label="" class="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800">Learn more</a>
         </div>
       </div>
-      <div>
-        <h6 class="mb-2 font-semibold leading-5 uppercase">We innovate</h6>
-        <p class="mb-3 text-sm text-gray-900">
-          When you’re the leader in your field, taking calculated risks is part of the landscape. Our subject matter expertise provides our clients with extensive technological knowledge that is combined with proven real-world deployment experience that you’ll see in every project we take on.  Our extensive toolbox is wide, deep, and well equipped with expert people and systems that are ready to take on any challenge.
-        </p>
+      <div class="flex flex-col max-w-md sm:mx-auto sm:flex-row">
+        <div class="mr-4">
+          <div class="flex items-center justify-center w-12 h-12 mb-4 rounded-full bg-indigo-50">
+            <svg class="w-10 h-10 text-deep-purple-accent-400" stroke="currentColor" viewBox="0 0 52 52">
+              <polygon stroke-width="3" stroke-linecap="round" stroke-linejoin="round" fill="none" points="29 13 14 29 25 29 23 39 38 23 27 23"></polygon>
+            </svg>
+          </div>
+        </div>
+        <div>
+          <h6 class="mb-3 text-xl font-bold leading-5">We do</h6>
+          <p class="mb-3 text-sm text-gray-900">
+            When you’re the leader in your field, taking calculated risks is part of the landscape. Our subject matter expertise provides our clients with extensive technological knowledge that is combined with proven real-world deployment experience that you’ll see in every project we take on.  Our extensive toolbox is wide, deep, and well equipped with expert people and systems that are ready to take on any challenge.
 
+          </p>
+          <a href="/" aria-label="" class="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800">Learn more</a>
+        </div>
+      </div>
+      <div class="flex flex-col max-w-md sm:mx-auto sm:flex-row">
+        <div class="mr-4">
+          <div class="flex items-center justify-center w-12 h-12 mb-4 rounded-full bg-indigo-50">
+            <svg class="w-10 h-10 text-deep-purple-accent-400" stroke="currentColor" viewBox="0 0 52 52">
+              <polygon stroke-width="3" stroke-linecap="round" stroke-linejoin="round" fill="none" points="29 13 14 29 25 29 23 39 38 23 27 23"></polygon>
+            </svg>
+          </div>
+        </div>
+        <div>
+          <h6 class="mb-3 text-xl font-bold leading-5">We innovate </h6>
+          <p class="mb-3 text-sm text-gray-900">
+            UTS fully understands today’s fast pace of business and how quickly technology can change.  With this in mind, our ‘design forward’ techniques help to ‘future proof’ solutions we provide to our clientele. ‘Future proofing’ solutions we build today allows those solutions to be more easily modified ‘down the road’ to meet future business needs.
 
-        <a href="/" aria-label="" class="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800">Learn more</a>
+            Innovation at UTS is evidenced by our numerous issued patents, and patents pending, in the field of electric / hybrid electric energy and storage systems, as well as other related technology areas. When you work with UTS, your solutions will also have the latest technological advances integrated as well.          </p>
+          <a href="/" aria-label="" class="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800">Learn more</a>
+        </div>
       </div>
     </div>
   </div>
-</div>
 
 </Section>
 </template>
