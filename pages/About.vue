834a7bc7bf07f865d0b2100a39492375e9505091
unknown <bensonagala@live.com>
2023-02-07 10:59:18 +0300

    reworks on some pages

--- pages/About.vue
+++ pages/About.vue
@@ -1,15 +1,15 @@
 <template>
     <Section>
       <Navbar/>
-      <div class="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
-        <div class="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
+      <div class="px-4 py-28 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+        <div class="max-w-xl my-16 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
       <div>
-      <p class="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-accent-400">
+    <!--   <p class="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-accent-400">
         Brand new
-      </p>
+      </p> -->
     </div>
-    <h2 class="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
-      <span class="relative inline-block">
+    <h2 class=" mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto uppercase">
+ <!--      <span class="relative inline-block">
         <svg viewBox="0 0 52 24" fill="currentColor" class="absolute top-0 left-0 z-0 hidden w-32 -mt-8 -ml-20 text-blue-gray-100 lg:w-32 lg:-ml-28 lg:-mt-10 sm:block">
           <defs>
             <pattern id="fdca20a0-aeb4-4caf-ba1b-4351eee42363" x="0" y="0" width=".135" height=".30">
@@ -17,14 +17,14 @@
             </pattern>
           </defs>
           <rect fill="url(#fdca20a0-aeb4-4caf-ba1b-4351eee42363)" width="52" height="24"></rect>
-        </svg>
-        <span class="relative">The</span>
-      </span>
-      quick, brown fox jumps over a lazy dog
+        </svg> -->
+    
+      <!-- </span> -->
+      What our <span class="text-purple-600">  experience</span> and knowhow can do to you and your business
     </h2>
-    <p class="text-base text-gray-700 md:text-lg">
+  <!--   <p class="text-base text-gray-700 md:text-lg">
       Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque rem aperiam, eaque ipsa quae.
-    </p>
+    </p> -->
   </div>
   <div class="grid max-w-md gap-8 row-gap-10 sm:mx-auto lg:max-w-full lg:grid-cols-3">
     <div class="flex flex-col sm:flex-row">
@@ -36,11 +36,11 @@
         </div>
       </div>
       <div>
-        <h6 class="mb-2 font-semibold leading-5">The deep ocean</h6>
+        <h6 class="mb-2 font-semibold leading-5 uppercase">We listen</h6>
         <p class="mb-3 text-sm text-gray-900">
-          A flower in my garden, a mystery in my panties. Heart attack never stopped old Big Bear.
+          Our success in this business is not only based on superior technical aptitude, it also includes our keen ability to carefully listen to all the client’s needs and concerns in order to provide exceptional solutions to their unique challenges.  You’re the expert in your market, and UTS provides the technical expertise needed to get your job done right – the first time – without getting in your way.
         </p>
-        <ul class="mb-4 -ml-1 space-y-2">
+       <!--  <ul class="mb-4 -ml-1 space-y-2">
           <li class="flex items-start">
             <span class="mr-1">
               <svg class="w-5 h-5 mt-px text-deep-purple-accent-400" stroke="currentColor" viewBox="0 0 52 52">
@@ -65,7 +65,7 @@
             </span>
             Reference
           </li>
-        </ul>
+        </ul> -->
         <a href="/" aria-label="" class="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800">Learn more</a>
       </div>
     </div>
@@ -78,11 +78,11 @@
         </div>
       </div>
       <div>
-        <h6 class="mb-2 font-semibold leading-5">When has justice</h6>
+        <h6 class="mb-2 font-semibold leading-5 uppercase">We know</h6>
         <p class="mb-3 text-sm text-gray-900">
-          Rough pomfret lemon shark plownose chimaera southern sandfish kokanee northern sea robin Antarctic cod.
+          Our years of experience entail working with large and small companies alike.  We know that executing to our client’s needs really means providing a uniquely focused solution, while meeting their schedule and budget constraints.  The UTS Team not only includes our in-house experts, it also includes hundreds of top notch specialty partner organizations that will ‘go the distance’ to assist UTS to deliver unique solutions that get the job done right, regardless of project scale.
         </p>
-        <ul class="mb-4 -ml-1 space-y-2">
+       <!--  <ul class="mb-4 -ml-1 space-y-2">
           <li class="flex items-start">
             <span class="mr-1">
               <svg class="w-5 h-5 mt-px text-deep-purple-accent-400" stroke="currentColor" viewBox="0 0 52 52">
@@ -107,7 +107,7 @@
             </span>
             Reference
           </li>
-        </ul>
+        </ul> -->
         <a href="/" aria-label="" class="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800">Learn more</a>
       </div>
     </div>
@@ -120,11 +120,11 @@
         </div>
       </div>
       <div>
-        <h6 class="mb-2 font-semibold leading-5">Organically grow</h6>
+        <h6 class="mb-2 font-semibold leading-5 uppercase">We do</h6>
         <p class="mb-3 text-sm text-gray-900">
-          A slice of heaven. O for awesome, this chocka full cuzzie is as rip-off as a deep cracker.
+          When you’re the leader in your field, taking calculated risks is part of the landscape. Our subject matter expertise provides our clients with extensive technological knowledge that is combined with proven real-world deployment experience that you’ll see in every project we take on.  Our extensive toolbox is wide, deep, and well equipped with expert people and systems that are ready to take on any challenge.
         </p>
-        <ul class="mb-4 -ml-1 space-y-2">
+       <!--  <ul class="mb-4 -ml-1 space-y-2">
           <li class="flex items-start">
             <span class="mr-1">
               <svg class="w-5 h-5 mt-px text-deep-purple-accent-400" stroke="currentColor" viewBox="0 0 52 52">
@@ -149,7 +149,26 @@
             </span>
             Reference
           </li>
-        </ul>
+        </ul> -->
+
+        <a href="/" aria-label="" class="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800">Learn more</a>
+      </div>
+    </div>
+    <div class="flex flex-col sm:flex-row">
+      <div class="sm:mr-4">
+        <div class="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-indigo-50">
+          <svg class="w-12 h-12 text-deep-purple-accent-400" stroke="currentColor" viewBox="0 0 52 52">
+            <polygon stroke-width="3" stroke-linecap="round" stroke-linejoin="round" fill="none" points="29 13 14 29 25 29 23 39 38 23 27 23"></polygon>
+          </svg>
+        </div>
+      </div>
+      <div>
+        <h6 class="mb-2 font-semibold leading-5 uppercase">We innovate</h6>
+        <p class="mb-3 text-sm text-gray-900">
+          When you’re the leader in your field, taking calculated risks is part of the landscape. Our subject matter expertise provides our clients with extensive technological knowledge that is combined with proven real-world deployment experience that you’ll see in every project we take on.  Our extensive toolbox is wide, deep, and well equipped with expert people and systems that are ready to take on any challenge.
+        </p>
+
+
         <a href="/" aria-label="" class="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800">Learn more</a>
       </div>
     </div>
